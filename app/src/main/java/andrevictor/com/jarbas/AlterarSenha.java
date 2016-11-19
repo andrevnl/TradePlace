eb824567d45fe6c8980fc7619c97f5f9a54ee6b9
André Victor do Nascimento Leite <andrevnl@hotmail.com>
2016-11-18 22:20:48 -0200

    Telas Quase la

---
+++ app/src/main/java/andrevictor/com/jarbas/AlterarSenha.java
@@ -0,0 +1,13 @@
+package andrevictor.com.jarbas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlterarSenha extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alterar_senha);
+    }
+}
